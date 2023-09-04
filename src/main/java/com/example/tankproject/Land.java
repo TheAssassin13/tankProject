e4aa71c9744484d5d67f027e9d56a2df932a16ed
Benjamin Farias <bfarias22@alumnos.utalca.cl>
2023-09-04 19:01:02 -0300

    code(Land.java): Development in class Fields added Methods added

--- src/main/java/com/example/tankproject/Land.java
+++ src/main/java/com/example/tankproject/Land.java
@@ -1,4 +1,23 @@
 package com.example.tankproject;
 
+import java.util.ArrayList;
+
 public class Land {
+    public ArrayList<ArrayList<Integer>> resolutionMatrix;
+    int height;
+    int width;
+
+    public Land(int height, int width) {
+        this.height = height;
+        this.width = width;
+        resolutionMatrix = new ArrayList<>(height);
+        for (int i = 0; i < height; i++) {
+            resolutionMatrix.add(new ArrayList<>(width));
+        }
+    }
+
+    public void terrainGeneration(int seaLevel) {
+        int rand = (int) (seaLevel * Math.random());
+        System.out.println(rand);
+    }
 }
