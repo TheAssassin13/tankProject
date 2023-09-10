ab9e5676130c9ee46b3c54babc1cdb6d8387dd12
vicetr3s <vicediazcastex@gmail.com>
2023-09-10 14:23:09 -0400

    feat: Tank disappears when hit
    ArrayList of players divided into dead and alive

--- src/main/java/com/example/tankproject/GameController.java
+++ src/main/java/com/example/tankproject/GameController.java
@@ -30,9 +30,9 @@ public class GameController implements Initializable {
     public GridPane buttonsPanel;
     public TextField angleTextField;
     public TextField powerTextField;
-
     public Terrain terrain;
-    public ArrayList<Player> players;
+    public ArrayList<Player> alivePlayers;
+    public ArrayList<Player> deadPlayers;
     public Text maxHeightTextField;
     public int maxHeight;
     public Text maxDistanceTextField;
@@ -46,10 +46,11 @@ public class GameController implements Initializable {
         //grid.heightProperty().bind(vbox.heightProperty());
         this.gc = grid.getGraphicsContext2D();
         //Test player
-        this.players = new ArrayList<>();
-        this.players.add(new Player("Player1", Color.GREENYELLOW, new Tank(Color.GREENYELLOW, new Point(0, 0))));
-        this.players.add(new Player("Player2", Color.BLUE, new Tank(Color.BLUE, new Point(0, 0))));
-        this.turn = players.get(0);
+        this.alivePlayers = new ArrayList<>();
+        this.deadPlayers = new ArrayList<>();
+        this.alivePlayers.add(new Player("Player1", Color.GREENYELLOW, new Tank(Color.GREENYELLOW, new Point(0, 0))));
+        this.alivePlayers.add(new Player("Player2", Color.BLUE, new Tank(Color.BLUE, new Point(0, 0))));
+        this.turn = alivePlayers.get(0);
         this.maxHeightTextField.setText("Max distance = 0");
         this.maxDistanceTextField.setText("Max height = 0");
         this.terrain = new Terrain(Constants.CANVAS_HEIGHT, Constants.WINDOWS_WIDTH);
@@ -70,7 +71,7 @@ public class GameController implements Initializable {
         gc.clearRect(0, 0, Constants.WINDOWS_WIDTH, Constants.WINDOWS_HEIGHT);
         Illustrator.drawBackground(this.gc);
         Illustrator.drawTerrain(this.gc, this.terrain);
-        for (Player p : this.players) {
+        for (Player p : this.alivePlayers) {
             Illustrator.drawTank(this.gc, p.tank);
         }
     }
@@ -83,8 +84,8 @@ public class GameController implements Initializable {
             posXSecondTank = Constants.WINDOWS_WIDTH - (Constants.TANK_SIZE * 2);
         int posYSecondTank = Constants.CANVAS_HEIGHT - Constants.TANK_SIZE;
         int posYFirstTank = Constants.CANVAS_HEIGHT - Constants.TANK_SIZE;
-        this.players.get(0).tank.position.setX(posXFirstTank);
-        this.players.get(1).tank.position.setX(posXSecondTank);
+        this.alivePlayers.get(0).tank.position.setX(posXFirstTank);
+        this.alivePlayers.get(1).tank.position.setX(posXSecondTank);
 
         //Position tank 1 on terrain
         for (int i = 0; i < Constants.CANVAS_HEIGHT; i++) {
@@ -93,7 +94,7 @@ public class GameController implements Initializable {
                 break;
             }
         }
-        this.players.get(0).tank.position.setY(posYFirstTank);
+        this.alivePlayers.get(0).tank.position.setY(posYFirstTank);
 
         //Position tank 2 on terrain
         for (int i = 0; i < Constants.CANVAS_HEIGHT; i++) {
@@ -102,18 +103,22 @@ public class GameController implements Initializable {
                 break;
             }
         }
-        this.players.get(1).tank.position.setY(posYSecondTank);
+        this.alivePlayers.get(1).tank.position.setY(posYSecondTank);
     }
 
-    public boolean tanksCollision(Shot shot) {
-        for (Player p : this.players) {
+    public Player tanksCollision(Shot shot) {
+        for (Player p : this.alivePlayers) {
             if (this.turn != p && shot.tankCollision(p.tank)) {
-                return true;
+                return p;
             }
         }
-        return false;
+        return null;
     }
 
+    public void deleteDeadPlayer(Player player) {
+        this.deadPlayers.add(player);
+        this.alivePlayers.remove(player);
+    }
 
     public void onShootButtonClick(ActionEvent actionEvent) {
         maxHeight = 0;
@@ -122,33 +127,31 @@ public class GameController implements Initializable {
         if (!powerTextField.getText().isEmpty() && !angleTextField.getText().isEmpty()) {
             Shot s = new Shot(new Point(turn.tank.position.getX(), turn.tank.position.getY()), Double.parseDouble(powerTextField.getText()), Double.parseDouble(angleTextField.getText()));
             maxHeight = (int) ((Math.pow(s.initialVelocity,2) * Math.pow(Math.sin(s.angle),2)) / (2 * Constants.GRAVITY));
-            maxDistance = (int) ((Math.pow(s.initialVelocity,2) * Math.sin(s.angle * 2)) / Constants.GRAVITY);
-            maxDistanceTextField.setText("Max height = " + maxHeight);
+            maxDistance = (int) Math.abs(((Math.pow(s.initialVelocity,2) * Math.sin(s.angle * 2)) / Constants.GRAVITY));
             maxHeightTextField.setText("Max distance = " + maxDistance);
+            maxDistanceTextField.setText("Max height = " + maxHeight);
             new AnimationTimer() {
                 @Override
                 public void handle(long now) {
-
                     s.shotPosition();
                     drawingMethods();
                     Illustrator.drawShot(gc, s);
                     if (s.position.getX() > Constants.WINDOWS_WIDTH || s.position.getY() > Constants.CANVAS_HEIGHT) {
                         stop();
                         changeTurn();
-
                     }
-                    if (tanksCollision(s)) System.out.println("Impacto");
+                    if (tanksCollision(s) != null) {
+                        System.out.println(tanksCollision(s).name + " se muricio");
+                        deleteDeadPlayer(tanksCollision(s));
+                    }
                     if (s.terrainCollision(terrain)) {
                         stop();
                         changeTurn();
                         s.shotPosition();
                         drawingMethods();
-
                     }
                 }
             }.start();
-
-
         }
         angleTextField.clear();
         powerTextField.clear();
@@ -156,12 +159,12 @@ public class GameController implements Initializable {
     }
 
     public void changeTurn() {
-        for (int i = 0; i < this.players.size(); i++) {
-            if (this.turn == this.players.get(i)) {
-                if (i + 1 < this.players.size()) {
-                    this.turn = this.players.get(i + 1);
+        for (int i = 0; i < this.alivePlayers.size(); i++) {
+            if (this.turn == this.alivePlayers.get(i)) {
+                if (i + 1 < this.alivePlayers.size()) {
+                    this.turn = this.alivePlayers.get(i + 1);
                 } else {
-                    this.turn = this.players.get(0);
+                    this.turn = this.alivePlayers.get(0);
                 }
                 break;
             }
